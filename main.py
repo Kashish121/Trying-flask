4680c6e07c482e119e02ecd39ad148c06641b8c9
Kashish121 <56366103+Kashish121@users.noreply.github.com>
2020-06-20 02:34:48 +0530

    Flask connectivity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,17 +9,20 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
-@app.route('/input', methods=["GET"])
+@app.route('/input', methods=["GET", "POST"])
 
 def input():
-    # if request.method == "POST":
-        # host = request.form["host"]
-        # range_low = request.form["range_low"]
-        # range_high = request.form["range_high"]
-        host = "localhost"
-        range_low = "43"
-        range_high = "66"
+    if request.method == "GET":
+        # val = request.form["val"]
+        host = request.form["host"]
+        range_low = request.form["range_low"]
+        range_high = request.form["range_high"]
+        # host = "localhost"
+        # range_low = "43"
+        # range_high = "66"
         return render_template('index.html', host=host, range_low=range_low, range_high=range_high)
+        # return redirect(url_for('input'))
+        # return '%s %s %s' %(host, range_low, range_high)
     # return render_template('index.html')
 
 
